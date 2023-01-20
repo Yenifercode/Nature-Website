a146d0888a7f9b5fd1b6d8738dd2b5cc68619eb2
Yenifer Tapia <yeni.flowerr23@gmail.com>
2023-01-20 10:20:08 -0500

    js,html,css

---
+++ Scripts/mountains.js
@@ -0,0 +1,43 @@
+function mountainImage(item){
+    const img = document.createElement("img");
+    img.src = "images/" + item.img;
+    return img;
+}
+
+function mountainTitle(item){
+    const text = document.createElement("div");
+    text.classList.add("card-title");
+    text.innerHTML = item.name;
+    return text;
+}
+
+function mountainCard(item) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.appendChild(mountainImage(item));    
+    card.appendChild(mountainTitle(item));
+    card.appendChild(mountainLevel(item));
+    card.appendChild(mountainInfo(item));
+    return card;
+}
+
+function mountainLevel(item) {
+    const level = document.createElement("p")
+    level.classList.add("card-mtnLevel");
+    level.innerHTML = "<b>Effort Level <b/><br>";
+    level.innerHTML += item.effort;
+    return level;
+
+}
+
+function mountainInfo(item) {
+    const info = document.createElement("p");
+    info.classList.add("card-description");
+    info.innerHTML= "<b>Description: </b><br>";
+    info.innerHTML += item.desc;
+    return info;
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    mountainsArray.forEach( m => window.results.appendChild(mountainCard(m)));
+});
